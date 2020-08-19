ed645d7fb76a12286a9ebb48750af6d8bdb7c655
Kha Nguyen <khanguyen@appvity.com>
2020-08-19 11:57:12 +0700

    Design UI login & register

---
+++ Messenger/Resources/Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  Extensions.swift
+//  Messenger
+//
+//  Created by apple on 8/19/20.
+//  Copyright © 2020 Kha Nguyen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+       
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+    
+}
